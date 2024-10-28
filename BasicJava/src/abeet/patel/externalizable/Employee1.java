24c63d831d89c06c855dffa919a529d0e5d1c8cd
abeetpatel <abeetpatel@outlook.com>
2024-10-29 00:02:51 +0530

    '29-10-2024'

---
+++ BasicJava/src/abeet/patel/externalizable/Employee1.java
@@ -0,0 +1,40 @@
+package abeet.patel.externalizable;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Employee1 implements Externalizable {
+	
+	String name;
+	transient int id;
+	
+	public Employee1(String n, int id) {
+		 
+		this.name = n;
+		this.id = id;
+		
+	}
+	
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeObject(name);
+		out.writeInt(id);
+		
+	}
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		name = (String) in.readObject();
+		id = in.readInt();
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Employee1 [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+
+}
---
+++ BasicJava/src/abeet/patel/externalizable/OutputStreamOfExternalizable.java
@@ -0,0 +1,13 @@
+package abeet.patel.externalizable;
+
+public class OutputStreamOfExternalizable {
+	
+	public static void main(String[] args) {
+		
+		Employee1 e1 = new Employee1("akhilesh", 15);
+		
+		File
+		
+	}
+
+}
